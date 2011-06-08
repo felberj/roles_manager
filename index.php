5b84a4d5ac311d5a965acc010b53af7b8cc581b3
andrewmman <andrewmman@macmini.local>
2011-06-08 19:44:44 +0200

    change info

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <?php
 
 /**
-  *  Roles Manager plugin pre-release for Wolf CMS
-  *  Available on the forum http://bit.ly/wf_roles_manager
+  *  Roles Manager plugin for Wolf CMS
   * 
   *  Manage Roles and assign/remove permissions.
   *
   *  @author andrewmman <andrewmman@gmail.com>
   *  @package Plugins
   *  @subpackage roles_manager
-  *  @version 0.0.1
+  *  @version 0.1.1
   *  @copyright andrewmman, 2011
   *  @license http://www.gnu.org/licenses/gpl.html GPLv3 license
   */
@@ -17,13 +16,13 @@
 if (!defined('IN_CMS')) { exit(); }
 
 Plugin::setInfos(array(
-	'id'	=> 'roles_manager',
-	'title'	=> __('Roles Manager'),
-	'description'	=> __('Manage Roles and their permissions.'),
-	'version'	=> '0.1.1',
-	'license'	=> 'GPL',
-	'author'	=> 'andrewmman',
-    'update_url'  => 'http://dev.molindeadela.es/plugins.xml',
+    'id'	=> 'roles_manager',
+    'title'	=> __('Roles Manager'),
+    'description'	=> __('Manage Roles and their permissions.'),
+    'version'	=> '0.1.1',
+    'license'	=> 'GPL',
+    'author'	=> 'andrewmman',
+    'update_url'  => 'http://andrewmman.byethost7.com/wolfplugins.xml',
     'website'     => 'http://www.wolfcms.org/forum/post9634.html',
     'require_wolf_version' => '0.7.5', 
     'type'	=> 'backend'
